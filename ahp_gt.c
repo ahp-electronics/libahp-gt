25baf7c38c800a3781e8f95c1c33533cdfece1b4
Ilia Platone <info@iliaplatone.com>
2022-11-22 01:15:21 +0100

    Remove useless gettimeofday() redefinition

--- ahp_gt.c
+++ ahp_gt.c
@@ -115,27 +115,6 @@ static unsigned int ahp_gt_detected[128] = { 0 };
 static gt1_info devices[128];
 static int sockfd;
 
-#ifdef _WIN32
-int gettimeofday(struct timeval * tp, struct timezone * tzp)
-{
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime(&system_time);
-    SystemTimeToFileTime(&system_time, &file_time);
-    time = ((uint64_t)file_time.dwLowDateTime);
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec = (long)((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
-    return 0;
-}
-#endif
-
 static double get_timestamp()
 {
     struct timezone tz;
