bd8d4d846339f9dae7cd3eab7d195ff2898354fd
Ilia Platone <info@iliaplatone.com>
2025-08-17 14:46:05 +0200

    Add tests.yml

--- brew.sh.cmake
+++ brew.sh.cmake
@@ -6,8 +6,8 @@ directory=$(cat ${CMAKE_CURRENT_SOURCE_DIR}/.git/config | grep origin -A 1 | hea
 repository=$(echo $directory | cut -d '/' -f 1)
 project=$(echo $directory | cut -d '/' -f 2)
 brew tap --force homebrew/core
-brew create --cmake https://github.com/$repository/$project/archive/refs/tags/v@AHPGT_VERSION@.tar.gz --set-name "$project@@AHPGT_VERSION@" --set-version '@AHPGT_VERSION@' --tap homebrew/core
-brew audit --new $project@@AHPGT_VERSION@
-brew install --build-from-source --verbose --debug $project@@AHPGT_VERSION@
-brew test $project@@AHPGT_VERSION@
-#Editing /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/lib/$project@@AHPGT_VERSION@.rb
+brew create --cmake https://github.com/$repository/$project/archive/refs/tags/v@AHPGT_VERSION@.tar.gz --set-name "$project" --set-version '@AHPGT_VERSION@' --tap homebrew/core
+brew audit --new $project
+brew install --build-from-source --verbose --debug $project
+brew test $project
+brew bottle /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/lib/$project.rb
